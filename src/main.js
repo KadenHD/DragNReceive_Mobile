7a79a08f4f4a0712f06f1f896847d6ab6d046f95
KadenHD <pierraored@gmail.com>
2022-03-21 19:21:34 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { IonicVue } from '@ionic/vue';
 import router from './router';
 import store from './store';
 import './plugins/axios.js';
-
-import { IonicVue } from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
