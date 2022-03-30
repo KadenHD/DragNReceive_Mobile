11bc8be1d4012f3017d2d8d560ca2acb606102b6
KadenHD <pierraored@gmail.com>
2022-03-30 20:51:10 +0200

    Create TopSection.vue

---
+++ src/components/auth/TopSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="auth__top__section">
+    <ion-card-header>
+      <ion-card-title>{{ title }}</ion-card-title>
+    </ion-card-header>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { IonCardHeader, IonCardTitle } from "@ionic/vue";
+
+export default defineComponent({
+  name: "AuthTopSection",
+  props: {
+    title: String,
+  },
+  components: {
+    IonCardHeader,
+    IonCardTitle,
+  },
+});
+</script>
