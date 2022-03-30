116a8e536efda7b91ec86ee924086bab4510e8db
KadenHD <pierraored@gmail.com>
2022-03-30 20:49:50 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,18 @@
 <template>
   <ion-app>
-    <ion-page>
-      <!--- if --->
-      <SearchBar v-if="currentUser" />
-      <!--- else --->
-      <LogoBar v-else />
-      <ion-content>
-        <ion-router-outlet />
-      </ion-content>
-      <!--- if logged --->
-      <Footer />
-    </ion-page>
+    <ion-router-outlet />
   </ion-app>
 </template>
 
 <script>
-import { IonApp, IonPage, IonContent, IonRouterOutlet } from "@ionic/vue";
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
 import { defineComponent } from "vue";
-
-import SearchBar from "@/components/SearchBar.vue";
-import LogoBar from "@/components/LogoBar.vue";
-import Footer from "@/components/Footer.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     IonApp,
-    IonPage,
-    IonContent,
     IonRouterOutlet,
-    SearchBar,
-    LogoBar,
-    Footer,
   },
 });
 </script>
