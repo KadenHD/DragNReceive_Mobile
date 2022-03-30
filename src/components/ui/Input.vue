f0c957973c429c185fbba7c8c391895e12e00a23
KadenHD <pierraored@gmail.com>
2022-03-30 20:51:16 +0200

    Create Input.vue

---
+++ src/components/ui/Input.vue
@@ -0,0 +1,27 @@
+<template>
+  <ion-item>
+    <ion-label position="floating">{{ label }}</ion-label>
+    <ion-input
+      :type="type"
+      :placeholder="placeholder"
+      :value="value"
+      @input="$emit('update:value', $event.target.value)"
+    ></ion-input>
+  </ion-item>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { IonItem, IonLabel, IonInput } from "@ionic/vue";
+
+export default defineComponent({
+  name: "UiInput",
+  props: {
+    label: String,
+    type: String,
+    placeholder: String,
+    value: String,
+  },
+  components: { IonItem, IonLabel, IonInput },
+});
+</script>
