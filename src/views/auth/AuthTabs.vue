8d4fb8fa164cf301f9c8d4eeae083ebdee2dab2f
KadenHD <pierraored@gmail.com>
2022-03-30 20:51:21 +0200

    Create AuthTabs.vue

---
+++ src/views/auth/AuthTabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <AuthHeader />
+      <ion-router-outlet></ion-router-outlet>
+      <AuthFooter />
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script>
+import AuthHeader from "@/components/auth/Header.vue";
+import AuthFooter from "@/components/auth/Footer.vue";
+import { IonPage, IonTabs, IonRouterOutlet } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AuthTabs",
+  components: {
+    AuthHeader,
+    AuthFooter,
+    IonPage,
+    IonTabs,
+    IonRouterOutlet,
+  },
+});
+</script>
