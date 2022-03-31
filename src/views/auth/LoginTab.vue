d05744b51e123097c525ccc56f205254d4f080e7
KadenHD <pierraored@gmail.com>
2022-03-31 19:18:06 +0200

    Update LoginTab.vue

--- src/views/auth/LoginTab.vue
+++ src/views/auth/LoginTab.vue
@@ -1,35 +1,40 @@
 <template>
   <AuthWrapper title="Connexion">
-    <UiInput
-      label="Adresse Email"
-      type="text"
-      placeholder="Saisissez votre adresse email"
-      v-model:value="data.email"
-    />
-    <UiInput
-      label="Mot de passe"
-      type="password"
-      placeholder="Saisissez votre mot de passe"
-      v-model:value="data.password"
-    />
-    <UiButton color="primary" :form="validForm" @click="login()"
-      >Se connecter</UiButton
-    >
+    <form @submit.prevent="submitLoginForm">
+      <UiInput
+        label="Adresse Email"
+        type="text"
+        placeholder="Saisissez votre adresse email"
+        v-model:value="data.email"
+        :valid="emailIsValid(data.email).valid"
+        :error="emailIsValid(data.email).error"
+      />
+      <UiInput
+        label="Mot de passe"
+        type="password"
+        placeholder="Saisissez votre mot de passe"
+        v-model:value="data.password"
+        :valid="passwordIsValid(data.password).valid"
+        :error="passwordIsValid(data.password).error"
+      />
+      <UiButton color="primary" type="submit">Se connecter</UiButton>
+    </form>
   </AuthWrapper>
 </template>
 
 <script>
 import AuthWrapper from "@/components/auth/Wrapper.vue";
-import UiButton from "@/components/ui/Button.vue";
 import UiInput from "@/components/ui/Input.vue";
+import UiButton from "@/components/ui/Button.vue";
 import { defineComponent, reactive } from "vue";
+import { emailIsValid, passwordIsValid } from "@/utils/validInputs.js";
 
 export default defineComponent({
   name: "AuthLoginTab",
   components: {
     AuthWrapper,
-    UiButton,
     UiInput,
+    UiButton,
   },
   setup() {
     window.onload = function () {
@@ -39,10 +44,21 @@ export default defineComponent({
       email: "",
       password: "",
     });
-    const login = () => {
-      console.log(data);
+    return {
+      data,
+      emailIsValid,
+      passwordIsValid,
     };
-    return { data, login, errors: {}, validForm: false };
+  },
+  methods: {
+    submitLoginForm() {
+      const formIsValid =
+        emailIsValid(this.data.email).valid &&
+        passwordIsValid(this.data.password).valid;
+      if (formIsValid) {
+        console.log("Valid");
+      }
+    },
   },
 });
 </script>
