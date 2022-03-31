fcd8e922f6831e0fb04f7d84f9b2e2c0a2e957fa
KadenHD <pierraored@gmail.com>
2022-03-31 19:17:58 +0200

    Update Input.vue

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -8,21 +8,23 @@
       @input="$emit('update:value', $event.target.value)"
     ></ion-input>
   </ion-item>
-  <slot></slot>
+  <ion-text v-if="!valid" color="danger">{{ error }} </ion-text>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { IonItem, IonLabel, IonInput } from "@ionic/vue";
+import { IonItem, IonLabel, IonInput, IonText } from "@ionic/vue";
 
 export default defineComponent({
   name: "UiInput",
   props: {
+    valid: Boolean,
+    error: String,
     label: String,
     type: String,
     placeholder: String,
     value: String,
   },
-  components: { IonItem, IonLabel, IonInput },
+  components: { IonItem, IonLabel, IonInput, IonText },
 });
 </script>
