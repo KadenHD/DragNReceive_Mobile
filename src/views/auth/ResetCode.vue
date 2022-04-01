9fbd2c7bcc7e224fc2e6403ba4e9aa694f64d5c3
KadenHD <pierraored@gmail.com>
2022-04-01 11:57:25 +0200

    Update ResetCode.vue

--- src/views/auth/ResetCode.vue
+++ src/views/auth/ResetCode.vue
@@ -1,105 +1,149 @@
 <template>
-  <ion-page>
-    <ion-content>
-      <ion-card>
-        <ion-card-content>
-          <ion-card-header>
-            <a href="/">Annuler</a>
-            <ion-title>Réinitialiser le mot de passe</ion-title>
-          </ion-card-header>
-          <ion-text>Un code a été envoyé à votre adresse email</ion-text><br />
-          <div class="ion-text-center">
-            <ion-text>Entrez le code</ion-text>
-          </div>
-          <ion-row>
-            <ion-col>
-              <ion-input
-                maxlength="1"
-                v-model="one"
-                @input="gotoNextField('two')"
-                inputmode="numeric"
-                class="rectangle one"
-              ></ion-input>
-            </ion-col>
-            <ion-col>
-              <ion-input
-                maxlength="1"
-                v-model="two"
-                @input="gotoNextField('three')"
-                inputmode="numeric"
-                class="rectangle two"
-              ></ion-input>
-            </ion-col>
-            <ion-col>
-              <ion-input
-                maxlength="1"
-                v-model="three"
-                @input="gotoNextField('four')"
-                inputmode="numeric"
-                class="rectangle three"
-              ></ion-input>
-            </ion-col>
-            <ion-col>
-              <ion-input
-                maxlength="1"
-                v-model="four"
-                inputmode="numeric"
-                class="rectangle four"
-              ></ion-input>
-            </ion-col>
-          </ion-row>
+  <AuthWrapper title="Réinitialisation de mot de passe">
+    <ion-text>Un code a été envoyé à votre adresse email !</ion-text>
+    <ion-text>Saisissez le code :</ion-text>
+    <form @submit.prevent="submitResetCodeForm">
+      <ion-row>
+        <ion-col>
+          <ion-input
+            type="number"
+            maxlength="1"
+            v-model="data.one"
+            inputmode="numeric"
+            class="formCode"
+          ></ion-input>
+        </ion-col>
+        <ion-col>
+          <ion-input
+            type="number"
+            maxlength="1"
+            v-model="data.two"
+            inputmode="numeric"
+            class="formCode"
+          ></ion-input>
+        </ion-col>
+        <ion-col>
+          <ion-input
+            type="number"
+            maxlength="1"
+            v-model="data.three"
+            inputmode="numeric"
+            class="formCode"
+          ></ion-input>
+        </ion-col>
+        <ion-col>
+          <ion-input
+            type="number"
+            maxlength="1"
+            v-model="data.four"
+            inputmode="numeric"
+            class="formCode"
+          ></ion-input>
+        </ion-col>
+      </ion-row>
 
-          <a href="/"> Button </a>
-        </ion-card-content>
-      </ion-card>
-    </ion-content>
-  </ion-page>
+      <UiInput
+        label="Mot de passe"
+        type="password"
+        placeholder="Saisissez votre mot de passe"
+        v-model:value="data.password"
+        :valid="passwordIsValid(data.password).valid"
+        :error="passwordIsValid(data.password).error"
+      />
+      <UiInput
+        label="Confirmez le mot de passe"
+        type="password"
+        placeholder="Saisissez de nouveau votre mot de passe"
+        v-model:value="data.confirmPassword"
+        :valid="
+          confirmPasswordIsValid(data.confirmPassword, data.password).valid
+        "
+        :error="
+          confirmPasswordIsValid(data.confirmPassword, data.password).error
+        "
+      />
+      <UiButton color="primary" type="submit">Changer</UiButton>
+    </form>
+  </AuthWrapper>
 </template>
 
 <script>
+import AuthWrapper from "@/components/auth/Wrapper.vue";
+import UiInput from "@/components/ui/Input.vue";
+import UiButton from "@/components/ui/Button.vue";
+import { IonRow, IonCol, IonText, IonInput } from "@ionic/vue";
+import { defineComponent, reactive } from "vue";
 import {
-  IonPage,
-  IonContent,
-  IonRow,
-  IonCol,
-  IonCard,
-  IonCardHeader,
-  IonCardContent,
-  IonTitle,
-  IonText,
-  IonInput,
-} from "@ionic/vue";
-export default {
-  setup() {
-    return {
-      one: null,
-      two: null,
-      three: null,
-      four: null,
-    };
-  },
+  passwordIsValid,
+  confirmPasswordIsValid,
+} from "@/utils/validInputs.js";
+import { alertController } from "@ionic/vue";
+
+export default defineComponent({
+  name: "AuthResetCode",
   components: {
-    IonPage,
-    IonContent,
+    AuthWrapper,
+    UiInput,
+    UiButton,
     IonRow,
     IonCol,
-    IonCard,
-    IonCardHeader,
-    IonCardContent,
-    IonTitle,
     IonText,
     IonInput,
   },
+  setup() {
+    const data = reactive({
+      one: "",
+      two: "",
+      three: "",
+      four: "",
+      password: "",
+      confirmPassword: "",
+    });
+    return { data, passwordIsValid, confirmPasswordIsValid };
+  },
   methods: {
-    gotoNextField(nextElement) {
-      nextElement.setFocus(); //eaze
+    submitResetCodeForm() {
+      const formIsValid =
+        !!this.data.one &&
+        !!this.data.two &&
+        !!this.data.three &&
+        !!this.data.four &&
+        passwordIsValid(this.data.password).valid &&
+        confirmPasswordIsValid(this.data.confirmPassword, this.data.password)
+          .valid;
+      if (formIsValid) {
+        this.submitResetCodeFormConfirm();
+      }
+    },
+    async submitResetCodeFormConfirm() {
+      const alert = await alertController.create({
+        header: "Souhaitez vous réellement modifier votre mot de passe ?",
+        message:
+          "Une fois modifié, vous pourrez de nouveau vous connecter à votre compte.",
+        buttons: [
+          {
+            text: "Annuler",
+            role: "cancel",
+            cssClass: "danger",
+            id: "cancel-button",
+          },
+          {
+            text: "Créer",
+            id: "confirm-button",
+            handler: () => {
+              console.log("Valid");
+            },
+          },
+        ],
+      });
+      return alert.present();
     },
   },
-};
+});
 </script>
 
-<style>
-.rectangle {
+<style lang="scss">
+.formCode {
   width: 50px;
   height: 50px;
   box-shadow: 0px 5px 25px rgba(42, 122, 227, 0.911);
