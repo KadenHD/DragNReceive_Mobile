d5b7f3bf47ca7d3fdc549e930c7cc9d9bd8858db
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-04 15:37:46 +0200

    Create HomeTab.vue

---
+++ src/views/HomeTab.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-page>
+    <a href="javascript:void(0)" @click="logoutClick">Déconnexion</a>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeTab",
+  components: {
+    IonPage,
+  },
+  methods: {
+    logoutClick() {
+      this.$store.dispatch("logout");
+    },
+  },
+});
+</script>
