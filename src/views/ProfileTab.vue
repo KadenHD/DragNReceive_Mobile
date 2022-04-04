2ee79516aa36b63da54bf788fe4529d1316327c1
KadenHD <pierraored@gmail.com>
2022-04-04 20:28:19 +0200

    Create ProfileTab.vue

---
+++ src/views/ProfileTab.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-page>
+    <a href="javascript:void(0)" @click="logoutClick">Déconnexion</a>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProfileTab",
+  components: {
+    IonPage,
+  },
+  setup() {
+    window.onload = function () {
+      document
+        .getElementById("tab-button-Profile")
+        .classList.add("tab-selected");
+    };
+  },
+  methods: {
+    logoutClick() {
+      this.$store.dispatch("logout");
+    },
+  },
+});
+</script>
