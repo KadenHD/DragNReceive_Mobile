4647e528710f686f2d85660a7114d4c6045e56cd
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-04 15:37:14 +0200

    Create Wrapper.vue

---
+++ src/components/auth/unlogged/Wrapper.vue
@@ -0,0 +1,48 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <div class="wrapper">
+        <ion-card>
+          <ion-card-content>
+            <ion-card-header>
+              <ion-card-title>{{ title }}</ion-card-title>
+            </ion-card-header>
+            <slot></slot>
+          </ion-card-content>
+        </ion-card>
+      </div> </ion-content
+  ></ion-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "AuthUnloggedWrapper",
+  props: {
+    title: String,
+  },
+  components: {
+    IonPage,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+  },
+});
+</script>
+
+<style lang="scss">
+div.wrapper {
+  margin: 40px 20px 80px 20px;
+}
+</style>
