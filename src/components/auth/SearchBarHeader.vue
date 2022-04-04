85f5d12f9983c16bfba048cad30af9deb89dc61c
KadenHD <pierraored@gmail.com>
2022-04-04 20:28:36 +0200

    Create SearchBarHeader.vue

---
+++ src/components/auth/SearchBarHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-searchbar
+        placeholder="Cherchez des produits / boutiques"
+      ></ion-searchbar>
+    </ion-toolbar>
+  </ion-header>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { IonHeader, IonToolbar, IonSearchbar } from "@ionic/vue";
+
+export default defineComponent({
+  name: "AuthSearchBarHeader",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonSearchbar,
+  },
+});
+</script>
