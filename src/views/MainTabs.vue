88952304492765ab79caaea01c806f6c19d76199
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-04 15:37:48 +0200

    Create MainTabs.vue

---
+++ src/views/MainTabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <Header />
+      <ion-router-outlet></ion-router-outlet>
+      <Footer />
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script>
+import Header from "@/components/auth/logged/Header.vue";
+import Footer from "@/components/auth/logged/Footer.vue";
+import { IonPage, IonTabs, IonRouterOutlet } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainTabs",
+  components: {
+    Header,
+    Footer,
+    IonPage,
+    IonTabs,
+    IonRouterOutlet,
+  },
+});
+</script>
