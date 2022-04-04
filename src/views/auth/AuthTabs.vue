fab680a8392cd5bbb5ad588e6c43d6a0ff35a12e
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-04 15:37:30 +0200

    Update AuthTabs.vue

--- src/views/auth/AuthTabs.vue
+++ src/views/auth/AuthTabs.vue
@@ -1,24 +1,24 @@
 <template>
   <ion-page>
     <ion-tabs>
-      <AuthHeader />
+      <AuthUnloggedHeader />
       <ion-router-outlet></ion-router-outlet>
-      <AuthFooter />
+      <AuthUnloggedFooter />
     </ion-tabs>
   </ion-page>
 </template>
 
 <script>
-import AuthHeader from "@/components/auth/Header.vue";
-import AuthFooter from "@/components/auth/Footer.vue";
+import AuthUnloggedHeader from "@/components/auth/unlogged/Header.vue";
+import AuthUnloggedFooter from "@/components/auth/unlogged/Footer.vue";
 import { IonPage, IonTabs, IonRouterOutlet } from "@ionic/vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "AuthTabs",
   components: {
-    AuthHeader,
-    AuthFooter,
+    AuthUnloggedHeader,
+    AuthUnloggedFooter,
     IonPage,
     IonTabs,
     IonRouterOutlet,
