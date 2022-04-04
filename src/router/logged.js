a622a163b9ea94f1098176ead82f4a89792557d7
KadenHD <pierraored@gmail.com>
2022-04-04 20:28:33 +0200

    Update logged.js

--- src/router/logged.js
+++ src/router/logged.js
@@ -16,6 +16,21 @@ export default [
                 name: 'Home',
                 component: () => import('@/views/HomeTab.vue'),
             },
+            {
+                path: 'profile',
+                name: 'Profile',
+                component: () => import('@/views/ProfileTab.vue'),
+            },
+            {
+                path: 'Cart',
+                name: 'Cart',
+                component: () => import('@/views/CartTab.vue'),
+            },
+            {
+                path: 'Tickets',
+                name: 'Tickets',
+                component: () => import('@/views/TicketsTab.vue'),
+            },
         ]
     },
 ];
