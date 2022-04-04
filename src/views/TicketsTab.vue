d2dcc6c881b252e8bb6c525a6e9d35a25c6981e1
KadenHD <pierraored@gmail.com>
2022-04-04 20:28:16 +0200

    Create TicketsTab.vue

---
+++ src/views/TicketsTab.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-page> Tickets </ion-page>
+</template>
+
+<script>
+import { IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TicketsTab",
+  components: {
+    IonPage,
+  },
+  setup() {
+    window.onload = function () {
+      document
+        .getElementById("tab-button-Tickets")
+        .classList.add("tab-selected");
+    };
+  },
+});
+</script>
