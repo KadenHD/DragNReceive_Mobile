0d86d38fa5dc554a428892d6cb97fef45ad1fd42
KadenHD <pierraored@gmail.com>
2022-04-05 20:31:16 +0200

    Update ProfileTab.vue

--- src/views/profile/ProfileTab.vue
+++ src/views/profile/ProfileTab.vue
@@ -1,28 +1,17 @@
 <template>
   <ion-page>
-    <ion-content :fullscreen="true" class="ion-padding">
-      <ion-button @click="setOpen(true)">Show Modal With Nav</ion-button>
-      <ion-modal :is-open="isOpenRef" @onDidDismiss="setOpen(false)">
-        <NavBaseModal :rootPage="ProfileParameters"></NavBaseModal>
-      </ion-modal>
-    </ion-content>
+    <a href="javascript:void(0)" @click="logoutClick">Déconnexion</a>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonPage, IonModal, IonButton } from "@ionic/vue";
-import { defineComponent, ref } from "vue";
-import NavBaseModal from "@/components/NavBaseModal.vue";
-import ProfileParameters from "@/views/profile/ProfileParameters.vue";
+import { IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "ProfileTab",
   components: {
-    IonContent,
     IonPage,
-    IonModal,
-    IonButton,
-    NavBaseModal,
   },
   setup() {
     window.onload = function () {
@@ -30,10 +19,11 @@ export default defineComponent({
         .getElementById("tab-button-Profile")
         .classList.add("tab-selected");
     };
-
-    const isOpenRef = ref(false);
-    const setOpen = (state) => (isOpenRef.value = state);
-    return { isOpenRef, setOpen, ProfileParameters };
+  },
+  methods: {
+    logoutClick() {
+      this.$store.dispatch("logout");
+    },
   },
 });
 </script>
