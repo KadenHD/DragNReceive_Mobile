561f6fae2205213129735776b7dcffeebec8dd35
KadenHD <pierraored@gmail.com>
2022-04-05 20:01:08 +0200

    Create test.vue

---
+++ src/views/profile/parameters/test.vue
@@ -0,0 +1,50 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons>
+          <ion-button @click="goBack">BACK</ion-button>
+        </ion-buttons>
+        <ion-title>MODAL HOME DETAIL</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h2>MODAL HOME DETAIL</h2>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonContent,
+  IonToolbar,
+  IonHeader,
+  IonTitle,
+  IonButton,
+  IonButtons,
+} from "@ionic/vue";
+export default defineComponent({
+  name: "ModalHomeDetail",
+  components: {
+    IonPage,
+    IonContent,
+    IonToolbar,
+    IonHeader,
+    IonTitle,
+    IonButton,
+    IonButtons,
+  },
+  props: ["modalNav"],
+  setup(props) {
+    const goBack = () => {
+      const nav = props.modalNav.value;
+      nav.pop();
+    };
+    return {
+      goBack,
+    };
+  },
+});
+</script>
