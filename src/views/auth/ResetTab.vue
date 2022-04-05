17a8583173c058a8f1bb79d98a655d1e08f92d8d
KadenHD <pierraored@gmail.com>
2022-04-05 20:01:03 +0200

    Update ResetTab.vue

--- src/views/auth/ResetTab.vue
+++ src/views/auth/ResetTab.vue
@@ -1,5 +1,5 @@
 <template>
-  <AuthUnloggedWrapper title="Réinitialisation de mot de passe">
+  <Wrapper title="Réinitialisation de mot de passe">
     <form @submit.prevent="submitResetForm">
       <UiInput
         label="Adresse Email"
@@ -11,11 +11,11 @@
       />
       <UiButton color="primary" type="submit">Envoyer</UiButton>
     </form>
-  </AuthUnloggedWrapper>
+  </Wrapper>
 </template>
 
 <script>
-import AuthUnloggedWrapper from "@/components/auth/unlogged/Wrapper.vue";
+import Wrapper from "@/components/Wrapper.vue";
 import UiInput from "@/components/ui/Input.vue";
 import UiButton from "@/components/ui/Button.vue";
 import { defineComponent, reactive } from "vue";
@@ -25,7 +25,7 @@ import { alertController } from "@ionic/vue";
 export default defineComponent({
   name: "AuthResetTab",
   components: {
-    AuthUnloggedWrapper,
+    Wrapper,
     UiInput,
     UiButton,
   },
