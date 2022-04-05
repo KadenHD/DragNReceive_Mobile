481e3b57e476e144b8b25f547de294aae91d9d6b
KadenHD <pierraored@gmail.com>
2022-04-05 20:01:12 +0200

    Create ProfileTab.vue

---
+++ src/views/profile/ProfileTab.vue
@@ -0,0 +1,39 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <ion-button @click="setOpen(true)">Show Modal With Nav</ion-button>
+      <ion-modal :is-open="isOpenRef" @onDidDismiss="setOpen(false)">
+        <NavBaseModal :rootPage="ProfileParameters"></NavBaseModal>
+      </ion-modal>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonContent, IonPage, IonModal, IonButton } from "@ionic/vue";
+import { defineComponent, ref } from "vue";
+import NavBaseModal from "@/components/NavBaseModal.vue";
+import ProfileParameters from "@/views/profile/ProfileParameters.vue";
+
+export default defineComponent({
+  name: "ProfileTab",
+  components: {
+    IonContent,
+    IonPage,
+    IonModal,
+    IonButton,
+    NavBaseModal,
+  },
+  setup() {
+    window.onload = function () {
+      document
+        .getElementById("tab-button-Profile")
+        .classList.add("tab-selected");
+    };
+
+    const isOpenRef = ref(false);
+    const setOpen = (state) => (isOpenRef.value = state);
+    return { isOpenRef, setOpen, ProfileParameters };
+  },
+});
+</script>
