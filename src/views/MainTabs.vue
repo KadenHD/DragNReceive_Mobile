5f8d02ece0ae8c52e3e0e40e94d8ca737a03b816
KadenHD <pierraored@gmail.com>
2022-04-05 20:01:06 +0200

    Update MainTabs.vue

--- src/views/MainTabs.vue
+++ src/views/MainTabs.vue
@@ -1,24 +1,27 @@
 <template>
   <ion-page>
     <ion-tabs>
-      <Header />
+      <SearchBarHeader v-if="this.$route.name === 'Home'" />
+      <DefaultHeader v-else />
       <ion-router-outlet></ion-router-outlet>
-      <Footer />
+      <LoggedFooter />
     </ion-tabs>
   </ion-page>
 </template>
 
 <script>
-import Header from "@/components/auth/SearchBarHeader.vue";
-import Footer from "@/components/auth/logged/Footer.vue";
+import SearchBarHeader from "@/components/SearchBarHeader.vue";
+import DefaultHeader from "@/components/DefaultHeader.vue";
+import LoggedFooter from "@/components/LoggedFooter.vue";
 import { IonPage, IonTabs, IonRouterOutlet } from "@ionic/vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "MainTabs",
   components: {
-    Header,
-    Footer,
+    SearchBarHeader,
+    DefaultHeader,
+    LoggedFooter,
     IonPage,
     IonTabs,
     IonRouterOutlet,
