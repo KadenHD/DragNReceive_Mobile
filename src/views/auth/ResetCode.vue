555bc61835f42e08456c1794a7bd30ebc1b607c1
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-06 15:02:36 +0200

    Update ResetCode.vue

--- src/views/auth/ResetCode.vue
+++ src/views/auth/ResetCode.vue
@@ -1,77 +1,94 @@
 <template>
-  <Wrapper title="Réinitialisation de mot de passe">
-    <ion-text>Un code a été envoyé à votre adresse email !</ion-text><br />
-    <ion-text>Saisissez le code :</ion-text>
-    <form @submit.prevent="submitResetCodeForm">
-      <ion-row>
-        <ion-col>
-          <ion-input
-            type="tel"
-            maxlength="1"
-            v-model="data.one"
-            inputmode="numeric"
-            class="formCode"
-          ></ion-input>
-        </ion-col>
-        <ion-col>
-          <ion-input
-            type="tel"
-            maxlength="1"
-            v-model="data.two"
-            inputmode="numeric"
-            class="formCode"
-          ></ion-input>
-        </ion-col>
-        <ion-col>
-          <ion-input
-            type="tel"
-            maxlength="1"
-            v-model="data.three"
-            inputmode="numeric"
-            class="formCode"
-          ></ion-input>
-        </ion-col>
-        <ion-col>
-          <ion-input
-            type="tel"
-            maxlength="1"
-            v-model="data.four"
-            inputmode="numeric"
-            class="formCode"
-          ></ion-input>
-        </ion-col>
-      </ion-row>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons> <ion-back-button></ion-back-button> Annuler </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <Wrapper title="Réinitialisation de mot de passe">
+      <ion-text>Un code a été envoyé à votre adresse email !</ion-text><br />
+      <ion-text>Saisissez le code :</ion-text>
+      <form @submit.prevent="submitResetCodeForm">
+        <ion-row>
+          <ion-col>
+            <ion-input
+              type="tel"
+              maxlength="1"
+              v-model="data.one"
+              inputmode="numeric"
+              class="formCode"
+            ></ion-input>
+          </ion-col>
+          <ion-col>
+            <ion-input
+              type="tel"
+              maxlength="1"
+              v-model="data.two"
+              inputmode="numeric"
+              class="formCode"
+            ></ion-input>
+          </ion-col>
+          <ion-col>
+            <ion-input
+              type="tel"
+              maxlength="1"
+              v-model="data.three"
+              inputmode="numeric"
+              class="formCode"
+            ></ion-input>
+          </ion-col>
+          <ion-col>
+            <ion-input
+              type="tel"
+              maxlength="1"
+              v-model="data.four"
+              inputmode="numeric"
+              class="formCode"
+            ></ion-input>
+          </ion-col>
+        </ion-row>
 
-      <UiInput
-        label="Mot de passe"
-        type="password"
-        placeholder="Saisissez votre mot de passe"
-        v-model:value="data.password"
-        :valid="passwordIsValid(data.password).valid"
-        :error="passwordIsValid(data.password).error"
-      />
-      <UiInput
-        label="Confirmez le mot de passe"
-        type="password"
-        placeholder="Saisissez de nouveau votre mot de passe"
-        v-model:value="data.confirmPassword"
-        :valid="
-          confirmPasswordIsValid(data.confirmPassword, data.password).valid
-        "
-        :error="
-          confirmPasswordIsValid(data.confirmPassword, data.password).error
-        "
-      />
-      <UiButton color="primary" type="submit">Changer</UiButton>
-    </form>
-  </Wrapper>
+        <UiInput
+          label="Mot de passe"
+          type="password"
+          placeholder="Saisissez votre mot de passe"
+          v-model:value="data.password"
+          :valid="passwordIsValid(data.password).valid"
+          :error="passwordIsValid(data.password).error"
+        />
+        <UiInput
+          label="Confirmez le mot de passe"
+          type="password"
+          placeholder="Saisissez de nouveau votre mot de passe"
+          v-model:value="data.confirmPassword"
+          :valid="
+            confirmPasswordIsValid(data.confirmPassword, data.password).valid
+          "
+          :error="
+            confirmPasswordIsValid(data.confirmPassword, data.password).error
+          "
+        />
+        <UiButton color="primary" type="submit">Changer</UiButton>
+      </form>
+    </Wrapper>
+  </ion-page>
 </template>
 
 <script>
 import Wrapper from "@/components/Wrapper.vue";
 import UiInput from "@/components/ui/Input.vue";
 import UiButton from "@/components/ui/Button.vue";
-import { IonRow, IonCol, IonText, IonInput } from "@ionic/vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonRow,
+  IonCol,
+  IonText,
+  IonInput,
+} from "@ionic/vue";
 import { defineComponent, reactive } from "vue";
 import {
   passwordIsValid,
@@ -85,6 +102,11 @@ export default defineComponent({
     Wrapper,
     UiInput,
     UiButton,
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
     IonRow,
     IonCol,
     IonText,
