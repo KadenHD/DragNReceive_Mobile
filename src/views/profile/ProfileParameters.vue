8e2b6bff9dcf1a7922495f223e54f92ed42765b6
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-06 12:11:18 +0200

    Create ProfileParameters.vue

---
+++ src/views/profile/ProfileParameters.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-buttons>
+        <ion-back-button></ion-back-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+</template>
+
+<script>
+import { IonButtons, IonHeader, IonToolbar } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { IonButtons, IonHeader, IonToolbar },
+});
+</script>
