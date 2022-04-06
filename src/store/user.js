93eebac54e66ed662b362c955200586b108478c3
KadenHD <pierraored@gmail.com>
2022-04-06 19:23:17 +0200

    Create user.js

---
+++ src/store/user.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import router from '@/router';
+import store from '@/store';
+
+import renderAlert from '@/utils/renderAlert.js';
+
+export default {
+    state: {},
+    getters: {},
+    actions: {
+        profileChangePhoto(context, file) {
+            const userId = store.getters.currentUser.id;
+            const formData = new FormData();
+            formData.append("photo", file);
+            axios
+                .put("users/" + userId, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then((response) => {
+                    renderAlert(response.data.success);
+                    router.push({ name: 'Profile' });
+                })
+                .catch((error) => {
+                    renderAlert("alertError", error.response.data.error);
+                });
+        },
+    },
+    mutations: {}
+}
