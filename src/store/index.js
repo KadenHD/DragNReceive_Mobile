32f0448ee489bdfd456cc579d48e981833e38297
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-07 13:06:20 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
 import auth from './auth.js';
 import user from './user.js';
+import ticket from './ticket.js';
 import cart from './cart.js';
 
 const store = new Vuex.Store({
   modules: {
     auth,
     user,
+    ticket,
     cart,
   },
   plugins: [createPersistedState()]
