53dfdb0cd964c20896671bd93c7223bc84b6027f
KadenHD <pierraored@gmail.com>
2022-04-07 21:39:36 +0200

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,25 @@ export const reformatedDates = (key) => {
     const date = new Date(key);
     return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
 }
+
+export const ticketStatusName = (key) => {
+    if (key == "1") {
+        return "Ouvert";
+    } else if (key == "2") {
+        return "Clos";
+    }
+}
+
+export const orderStatusName = (key) => {
+    if (key == "1") {
+        return "Validée";
+    } else if (key == "2") {
+        return "En cours";
+    } else if (key == "3") {
+        return "Disponible";
+    } else if (key == "4") {
+        return "Récupérée";
+    } else if (key == "5") {
+        return "Annulée";
+    }
+}
