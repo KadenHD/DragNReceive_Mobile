13d581214174e6ac9ce32a510e9deab897877e86
KadenHD <pierraored@gmail.com>
2022-04-10 19:54:45 +0200

    Create ShopsCards.vue

---
+++ src/components/card/ShopsCards.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-for="shop in items" v-bind:key="shop.id">
+    <ion-item>
+      {{ shop }}
+    </ion-item>
+    <ion-item-divider />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { IonItem, IonItemDivider } from "@ionic/vue";
+
+export default defineComponent({
+  name: "ShopsCards",
+  props: {
+    items: Object,
+  },
+  components: {
+    IonItem,
+    IonItemDivider,
+  },
+  setup() {},
+  methods: {},
+});
+</script>
