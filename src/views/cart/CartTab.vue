bfdb04653db6952451d7b815efa398b0b48bc8ed
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-10 13:46:50 +0200

    Create CartTab.vue

---
+++ src/views/cart/CartTab.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-page> Cart </ion-page>
+</template>
+
+<script>
+import { IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CartTab",
+  components: {
+    IonPage,
+  },
+  setup() {},
+});
+</script>
