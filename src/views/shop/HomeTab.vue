3740a516e2ca5d2ab65bab98c12443b0771c325d
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-10 13:47:11 +0200

    Create HomeTab.vue

---
+++ src/views/shop/HomeTab.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-page> Home </ion-page>
+</template>
+
+<script>
+import { IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeTab",
+  components: {
+    IonPage,
+  },
+  setup() {},
+});
+</script>
