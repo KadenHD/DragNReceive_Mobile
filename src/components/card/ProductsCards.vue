13535c05022b890549efcc7707ea77030359637f
KadenHD <pierraored@gmail.com>
2022-04-10 19:54:43 +0200

    Create ProductsCards.vue

---
+++ src/components/card/ProductsCards.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-for="product in items" v-bind:key="product.id">
+    <ion-item>
+      {{ product }}
+    </ion-item>
+    <ion-item-divider />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { IonItem, IonItemDivider } from "@ionic/vue";
+
+export default defineComponent({
+  name: "ProductsCards",
+  props: {
+    items: Object,
+  },
+  components: {
+    IonItem,
+
+    IonItemDivider,
+  },
+  setup() {},
+  methods: {},
+});
+</script>
