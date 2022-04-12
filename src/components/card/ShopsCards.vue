621d2955cd4d45afd2c2a1cff7d03443fe2563f0
KadenHD <pierraored@gmail.com>
2022-04-12 20:11:57 +0200

    Update ShopsCards.vue

--- src/components/card/ShopsCards.vue
+++ src/components/card/ShopsCards.vue
@@ -2,25 +2,78 @@
   <div v-for="shop in items" v-bind:key="shop.id">
     <ion-item-divider v-if="shop.id !== items[0].id" />
     <ion-item>
-      {{ shop }}
+      <ion-card>
+        <ion-img
+          :src="shop.path ? url + shop.path : 'assets/img/default.svg'"
+        />
+        <ion-card-header>
+          <ion-card-subtitle
+            ><a :href="`mailto:${shop.email}`">{{
+              shop.email
+            }}</a></ion-card-subtitle
+          >
+          <ion-card-subtitle
+            ><a :href="`tel:${shop.phone}`">{{
+              shop.phone
+            }}</a></ion-card-subtitle
+          >
+          <ion-card-title>{{ shop.name }}</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+          Adresse de la boutique partenaire :
+          {{ shop.city }}, {{ shop.street }},
+          {{ shop.postal }}
+        </ion-card-content>
+        <UiButton
+          class="button"
+          color="secondary"
+          @click="
+            this.$router.push({
+              name: 'Shop',
+              params: { id: shop.id },
+            })
+          "
+          >Voir</UiButton
+        >
+      </ion-card>
     </ion-item>
   </div>
 </template>
 
 <script>
+import UiButton from "@/components/ui/Button.vue";
+import {
+  IonItemDivider,
+  IonItem,
+  IonCard,
+  IonImg,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+} from "@ionic/vue";
 import { defineComponent } from "vue";
-import { IonItem, IonItemDivider } from "@ionic/vue";
 
 export default defineComponent({
-  name: "ShopsCards",
+  name: "ProductsCards",
   props: {
     items: Object,
   },
   components: {
-    IonItem,
+    UiButton,
     IonItemDivider,
+    IonItem,
+    IonCard,
+    IonImg,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardContent,
   },
-  setup() {},
+  setup() {
+    const url = process.env.VUE_APP_URL;
+    return { url };
+  },
   methods: {},
 });
 </script>
@@ -29,5 +82,16 @@ export default defineComponent({
 ion-item {
   margin-top: 10px;
   margin-bottom: 10px;
+}
+ion-card {
+  margin: 0 auto;
+}
+ion-img {
+  max-height: 150px;
+  max-width: 150px;
+  margin: 0 auto;
+}
+.button {
+  margin: 0 auto;
 }
 </style>
