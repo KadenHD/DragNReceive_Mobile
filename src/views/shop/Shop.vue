74d8b293d2f6796c3abec16c94ef9e9fac07c955
KadenHD <pierraored@gmail.com>
2022-04-12 22:23:01 +0200

    Create Shop.vue

---
+++ src/views/shop/Shop.vue
@@ -0,0 +1,39 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons> <ion-back-button></ion-back-button> Annuler </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content> Shop </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonButtons,
+  IonHeader,
+  IonToolbar,
+  IonBackButton,
+  IonContent,
+} from "@ionic/vue";
+import { chevronForward } from "ionicons/icons";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProfileParameters",
+  components: {
+    IonPage,
+    IonButtons,
+    IonHeader,
+    IonToolbar,
+    IonBackButton,
+    IonContent,
+  },
+  setup() {
+    return { chevronForward };
+  },
+  methods: {},
+});
+</script>
