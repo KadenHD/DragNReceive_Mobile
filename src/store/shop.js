274a66e0a261da05f25dc13bc4e56f1d2d922683
KadenHD <pierraored@gmail.com>
2022-04-12 22:23:09 +0200

    Update shop.js

--- src/store/shop.js
+++ src/store/shop.js
@@ -7,14 +7,20 @@ export default {
     state: {
         shops: null,
         shop: null,
+        products: null,
+        product: null,
     },
     getters: {
         shops: (state) => { return state.shops; },
         shop: (state) => { return state.shop; },
+        products: (state) => { return state.products; },
+        product: (state) => { return state.product; },
     },
     actions: {
         shops(context, shops) { context.commit('shops', shops); },
         shop(context, shop) { context.commit('shop', shop); },
+        products(context, products) { context.commit('products', products); },
+        product(context, product) { context.commit('product', product); },
         setShop(context, id) {
             axios
                 .get("shops/" + id)
@@ -36,9 +42,32 @@ export default {
                     renderAlert(error.response.data.error);
                 });
         },
+        setProduct(context, id) {
+            axios
+                .get("products/" + id)
+                .then((response) => {
+                    context.commit("product", response.data);
+                })
+                .catch((error) => {
+                    renderAlert(error.response.data.error);
+                    router.push({ name: "Home" });
+                });
+        },
+        setProducts(context) {
+            axios
+                .get("products")
+                .then((response) => {
+                    context.commit("products", response.data);
+                })
+                .catch((error) => {
+                    renderAlert(error.response.data.error);
+                });
+        },
     },
     mutations: {
         shops(state, shops) { state.shops = shops; },
         shop(state, shop) { state.shop = shop; },
+        products(state, products) { state.products = products; },
+        product(state, product) { state.product = product; },
     }
 }
