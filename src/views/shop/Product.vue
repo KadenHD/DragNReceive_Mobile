c9f3649c0d82be11cf91128cac5ab5b5b04a7a71
KadenHD <pierraored@gmail.com>
2022-04-14 15:19:26 +0200

    Update Product.vue

--- src/views/shop/Product.vue
+++ src/views/shop/Product.vue
@@ -5,11 +5,47 @@
         <ion-buttons> <ion-back-button></ion-back-button> Annuler </ion-buttons>
       </ion-toolbar>
     </ion-header>
-    <ion-content> {{ myProduct }} </ion-content>
+    <ion-content>
+      <Wrapper v-if="myProduct" :title="myProduct.name">
+        <ion-img :src="url + myProduct.path" />
+        <ion-card-header>
+          <ion-card-subtitle>{{ myProduct.stock }} restant</ion-card-subtitle>
+          <ion-card-title>{{ myProduct.price }} €</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+          {{ myProduct.description }} <br />
+        </ion-card-content>
+        <form @submit.prevent="submitBuyForm">
+          <UiInput
+            disabled
+            label="Quantité"
+            type="number"
+            v-model:value="data.quantities"
+            :valid="stockIsValid(data.quantities, myProduct.stock).valid"
+            :error="stockIsValid(data.quantities, myProduct.stock).error"
+          />
+          <br />
+          <ion-icon
+            @click="addQuantities()"
+            size="large"
+            :icon="addCircleOutline"
+          />
+          <ion-icon
+            @click="subQuantities()"
+            size="large"
+            :icon="removeCircleOutline"
+          />
+          <UiButton color="secondary" type="submit">Ajouter au panier</UiButton>
+        </form>
+      </Wrapper>
+    </ion-content>
   </ion-page>
 </template>
 
 <script>
+import Wrapper from "@/components/Wrapper.vue";
+import UiInput from "@/components/ui/Input.vue";
+import UiButton from "@/components/ui/Button.vue";
 import {
   IonPage,
   IonButtons,
@@ -17,23 +53,60 @@ import {
   IonToolbar,
   IonBackButton,
   IonContent,
+  IonImg,
+  IonCardTitle,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardContent,
+  IonIcon,
 } from "@ionic/vue";
-import { chevronForward } from "ionicons/icons";
-import { defineComponent } from "vue";
+import {
+  chevronForward,
+  addCircleOutline,
+  removeCircleOutline,
+} from "ionicons/icons";
+import { defineComponent, reactive } from "vue";
 import { mapGetters } from "vuex";
+import { stockIsValid } from "@/utils/validInputs.js";
+import { alertController } from "@ionic/vue";
 
 export default defineComponent({
   name: "ProfileParameters",
   components: {
+    Wrapper,
+    UiInput,
+    UiButton,
     IonPage,
     IonButtons,
     IonHeader,
     IonToolbar,
     IonBackButton,
     IonContent,
+    IonImg,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardContent,
+    IonIcon,
   },
   setup() {
-    return { chevronForward };
+    const url = process.env.VUE_APP_URL;
+    const data = reactive({
+      quantities: "1",
+    });
+    return {
+      url,
+      data,
+      stockIsValid,
+      chevronForward,
+      addCircleOutline,
+      removeCircleOutline,
+    };
+  },
+  watch: {
+    $route() {
+      this.data.quantities = "1";
+    },
   },
   created() {
     this.$store.dispatch("setProduct", this.$route.params.id);
@@ -46,6 +119,40 @@ export default defineComponent({
       } else {
         return this.product;
       }
+    },
+  },
+  methods: {
+    addQuantities() {
+      const result = parseInt(this.data.quantities) + 1;
+      this.data.quantities = result.toString();
+    },
+    subQuantities() {
+      const result = parseInt(this.data.quantities) - 1;
+      this.data.quantities = result.toString();
+    },
+    submitBuyForm() {
+      const formIsValid = stockIsValid(
+        this.data.quantities,
+        this.myProduct.stock
+      ).valid;
+      if (formIsValid) {
+        this.submitBuyFormConfirm();
+      }
+    },
+    async submitBuyFormConfirm() {
+      const alert = await alertController.create({
+        header: `${this.data.quantities} ${this.myProduct.name} ajouté au panier !`,
+        message:
+          "Vous pourrez par la suite accéder à votre panier pour acheter ces produits.",
+        buttons: [
+          {
+            text: "Ok",
+            role: "confirm",
+            id: "confirm-button",
+          },
+        ],
+      });
+      return alert.present();
     },
   },
 });
