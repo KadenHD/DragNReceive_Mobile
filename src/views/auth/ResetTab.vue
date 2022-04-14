a610872008ae39cda6c75c6c0ada2450391f79ba
KadenHD <pierraored@gmail.com>
2022-04-14 15:19:34 +0200

    Update ResetTab.vue

--- src/views/auth/ResetTab.vue
+++ src/views/auth/ResetTab.vue
@@ -60,7 +60,6 @@ export default defineComponent({
           {
             text: "Annuler",
             role: "cancel",
-            cssClass: "danger",
             id: "cancel-button",
           },
           {
