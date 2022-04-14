28cb6a0756f2458e17d369697c5d0c1a108e8b48
KadenHD <pierraored@gmail.com>
2022-04-14 15:19:30 +0200

    Update PasswordChange.vue

--- src/views/profile/PasswordChange.vue
+++ src/views/profile/PasswordChange.vue
@@ -92,7 +92,6 @@ export default defineComponent({
           {
             text: "Annuler",
             role: "cancel",
-            cssClass: "danger",
             id: "cancel-button",
           },
           {
