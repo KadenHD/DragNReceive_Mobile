5b53745570069f969b7780171273d34f58f24d3d
KadenHD <pierraored@gmail.com>
2022-04-20 20:55:54 +0200

    Update Textarea.vue

--- src/components/ui/Textarea.vue
+++ src/components/ui/Textarea.vue
@@ -1,16 +1,18 @@
 <template>
-  <ion-item>
-    <ion-label position="floating">{{ label }}</ion-label>
-    <ion-textarea
-      :disabled="disabled"
-      :type="type"
-      :placeholder="placeholder"
-      :value="value"
-      @input="$emit('update:value', $event.target.value)"
-      rows="8"
-    ></ion-textarea>
-  </ion-item>
-  <ion-text v-if="!valid" color="danger">{{ error }} </ion-text>
+  <div>
+    <ion-item>
+      <ion-label position="floating">{{ label }}</ion-label>
+      <ion-textarea
+        :disabled="disabled"
+        :type="type"
+        :placeholder="placeholder"
+        :value="value"
+        @input="$emit('update:value', $event.target.value)"
+        rows="8"
+      ></ion-textarea>
+    </ion-item>
+    <ion-text v-if="!valid" color="danger">{{ error }} </ion-text>
+  </div>
 </template>
 
 <script>
